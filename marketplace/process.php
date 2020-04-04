1ba40a09cbeda9b3ceb54402b2d6f53d5817ea98
jennyyxzhang <62733616+jennyyxzhang@users.noreply.github.com>
2020-04-03 19:49:03 -0700

    Add files via upload

--- marketplace/process.php
+++ marketplace/process.php
@@ -34,10 +34,12 @@ if (isset($_POST['login'])){
         $query = mysqli_query($conn, "SELECT distinct customer1.* FROM Customer1 WHERE username = '$user' ") or die(mysqli_error($conn));
        
         if(mysqli_num_rows($query) > 0){
-            
-            $result = mysqli_query($conn, "SELECT transactiondetail.servicetype, transactiondetail.daterequested from customer1 left join customer2 on customer1.cardnumber = customer2.cardnumber
+    
+            $result = mysqli_query($conn, "SELECT transactiondetail.servicetype, transactiondetail.daterequested,
+             transactiondetail.providerid, transactiondetail.customerid from customer1 left join customer2 on customer1.cardnumber = customer2.cardnumber
             left join transactiondetail on customer1.customerid = transactiondetail.customerid where customer1.username = '$user' ");
-
+            
+    
             echo '<table align = "center" cellspacing = "10" cellpadding = "8">
             <tr> 
             <td align = left> <b> Service </b> </td>
@@ -45,8 +47,24 @@ if (isset($_POST['login'])){
             <tr>';
 
             while($row = mysqli_fetch_array($result)){
+               $_SESSION['custid'] = $row["customerid"];
+               $_SESSION['providerid'] = $row["providerid"];
                 echo '<tr><td align = "left">' . $row["servicetype"]
                . '</td><td align = "right">' . $row["daterequested"] . '</td><td align = "left">';
+               echo '<form action = "leave-review.php" method = "post">
+               <label for="review"> Your Review:</label>
+               <input type="text" id="textR" name="textR" >
+               <label for="rating"> Your Rating:</label>
+               <select id="rating" name="rating">
+               <option value="0">0</option>
+               <option value="1">1</option>
+               <option value="2">2</option>
+               <option value="3">3</option>
+               <option value="4">4</option>
+               <option value="5">5</option>
+                </select>
+               <input type = "submit" name = "leave-review" value = "Leave Review"  >
+               </form>';
 
                echo '</tr>';
             }
@@ -62,7 +80,9 @@ if (isset($_POST['login'])){
             </form>';
             
         } else {
-            echo "Invalid Username. Please try again, or create a new account";
+            echo "<p>Invalid Username. Please try again, or create a new account</p>";
+            echo "<p>Redirecting back to home page....</p>";
+            header('refresh:3; url = http://localhost/marketplace.php');
            
         }
 }
